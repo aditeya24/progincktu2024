14e94b445248b6f53e214b8843b3c7b0942e7571
aditeya24 <88970191+aditeya24@users.noreply.github.com>
2025-03-05 10:20:19 +0530

    Completed ArmstrongNumRange.c

--- 19_2_25/ArmstrongNumRange.c
+++ 19_2_25/ArmstrongNumRange.c
@@ -1,7 +1,23 @@
 #include<stdio.h>
-void main()
+int main()
 {
-int start,end,i,temp,rem,sum=0;
+int start,end,i,temp,rem,sum=0,cube=1;
 printf("Enter start and end of range:\n");
 scanf("%d%d",&start,&end);
-
+printf("Armstrong numbers:\n");
+for(i=start;i<=end;i++)
+{
+temp=i;
+sum=0;
+while(temp>0)
+{
+rem=temp%10;
+temp=temp/10;
+cube=rem*rem*rem;
+sum+=cube;
+}
+if(sum==i)
+printf("%d\n",i);
+}
+return 0;
+}
